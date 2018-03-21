45350f93051735ffb91ee154db2b675eb2d37d33
Marin Leontenko <marin.leontenko@gmail.com>
2018-03-21 11:43:50 +0100

    buttons

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
         const { id } = this.props.match.params;
-        this.props.fetchPost(id);
+        this.props.fetchPost(id);        
     }
 
     render() {
@@ -17,6 +18,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back To Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
